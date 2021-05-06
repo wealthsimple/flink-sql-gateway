7265e8cd51b716abb7e2200a23eb7929bfbadccd
zhuguangbin <zhuguangbin@360.cn>
2021-05-06 19:07:39 +0800

    fix checkstyle

--- src/main/java/com/ververica/flink/table/gateway/utils/PipelineOptimizer.java
+++ src/main/java/com/ververica/flink/table/gateway/utils/PipelineOptimizer.java
@@ -16,7 +16,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
